909b8fa5c8b1639e35237d3d2eece04fca05586b
Italo Vieira <ItaloV@pma.local>
2022-11-07 16:57:57 -0300

    [Italo: criacao dos componentes de buscar de pokemon, pokemon card e paginacao]

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getPokemonData, getPokemons } from './components/Api';
 
 import Navbar from './components/Navbar';
+import Pokedex from './components/Pokedex';
 import Searchbar from './components/Searchbar';
+
+
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const itensPerpage = 25
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true)
+      const data = await getPokemons(itensPerpage, itensPerpage * page);
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url)
+      })
+
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerpage))
+    } catch (error) {
+      console.log("fetchPokemons error:", error);
+    }
+  }
+  useEffect(() => {
+    console.log("carregou")
+    fetchPokemons();
+  }, [page])
+
   return (
     <div>
       <Navbar />
       <Searchbar />
-      <div className="App">
-
-      </div>
+      <Pokedex pokemons={pokemons}
+        loading={loading}
+        page={page}
+        totalPages={totalPages}
+        setPage={setPage}
+      />
     </div>
   );
 }
