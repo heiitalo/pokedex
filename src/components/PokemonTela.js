dde246ccce3c234b7635a6ee86e7dba2555ceeaa
Italo Vieira <ItaloV@pma.local>
2022-11-10 16:59:34 -0300

    [Feature Italo Task estilizacao basica]

--- src/components/PokemonTela.js
+++ src/components/PokemonTela.js
@@ -4,10 +4,39 @@ import { useLocation } from "react-router-dom";
 const PokemonTela = () => {
   const pokemon = useLocation();
   return (
-    <div>
-        {pokemon.state.name}
+    <div className="pokemon-tela">
+      <div className="pokemon-card-tela">
+        <div className="pokemomon-card-tela-header">
+          <h1> {pokemon.state.name}</h1>
+        </div>
+        <div className="pokemon-card-tela-body">
+          <div className="pokemon-tela-image">
+            <img src={pokemon.state.sprites.other.home.front_default} />
+          </div>
+          <div className="pokemon-type">
+            {pokemon.state.types.map((type, index) => {
+              return (
+                <div key={index} className="pokemon-type-text-tela">
+                  <h3>{type.type.name}</h3>
+                </div>
+              );
+            })}
+          </div>
+       </div>
+      </div>
+      <div className="pokemon-tela-moves">
+        {pokemon.state.moves.map((move,index) =>{
+            return (
+                <div key={index} className="pokemon-tela-move">
+                    <h3>{move.move.name}</h3>
+
+                </div>
+            )
+        })}
+
+      </div>
     </div>
-  )
+  );
 };
 
 export default PokemonTela;
